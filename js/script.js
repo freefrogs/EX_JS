22c4a5552d906c8aabfad29d38f99f0f5b407017
Justyna Biernacka <justyna.biernacka.1986@gmail.com>
2017-12-28 13:24:14 +0100

    Finish task 8.5

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var a = prompt('Enter triangle base');
+var h = prompt('Enter triangle height');
+var triangleArea = a * h / 2;
+
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
