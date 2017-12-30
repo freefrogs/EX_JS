221002b6b60234e7bcf982d40a152e90c8394ead
Justyna Biernacka <justyna.biernacka.1986@gmail.com>
2017-12-30 11:13:21 +0100

    Add small correction to both js function

--- js/script.js
+++ js/script.js
@@ -1,22 +1,22 @@
 var rowTree = prompt('Enter the number of rows of the tree');
 
 function drawTree(levelTree) {
-	for (var i = 0 ; i <= rowTree ; i++) {
+	for (var i = 0 ; i < rowTree ; i++) {
 		var star = '';
-		for (var j = 0 ; j < i ; j++) {
+		for (var j = 0 ; j <= i ; j++) {
 			star += '*';
 		}
 		console.log(star);
 	}
 }
-console.log('Christmes Tree 1');
-var chistmestree = drawTree(rowTree);
+console.log('Christmas Tree 1');
+var chistmastree = drawTree(rowTree);
 
 function drawTree2(levelTree2) {
 	for (var k = 0 ; k < rowTree ; k++) {
 		var gap = ' ',
 			star2 = '*';
-		gap = gap.repeat(rowTree - k);
+		gap = gap.repeat(rowTree - k-1);
 
 		for (var l = 0 ; l < k ; l++) {
 			star2 += '**';
@@ -24,5 +24,5 @@ function drawTree2(levelTree2) {
 		console.log(gap + star2);
 	}
 }
-console.log('Christmes Tree 2');
-var chistmestree2 = drawTree2(rowTree);
+console.log('Christmas Tree 2');
+var chistmastree2 = drawTree2(rowTree);
